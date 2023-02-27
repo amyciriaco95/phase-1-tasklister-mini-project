dd15238641b85d1a40be253622e6780b6ff27bca
amyciriaco95 <amyciriaco95@gmail.com>
2023-02-27 18:07:47 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+
+})
+
+const list = document.getElementById('tasks')
+
+function handleForm(task){
+  const newTask = document.createElement('li')
+  newTask.textContent = `${task}`
+
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  newTask.appendChild(btn)
+
+  list.append(newTask)
+}
